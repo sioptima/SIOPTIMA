1b61e74ac49286ded95bd5281e7ec21c25aab8e0
LUTHFI <luthfinaufalwibisono1@gmail.com>
2025-11-08 21:32:24 +0700

    next config update for prisma

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-  
+    outputFileTracingIncludes: {
+        // Ensure Prisma engines and wasm are bundled into server output
+        "/src/server/db": [
+          "./generated/client/libquery_engine*",
+          "./generated/client/query_engine*",
+          "./generated/client/schema.prisma",
+          "./generated/client/*.node",
+          "./generated/client/*.wasm",
+        ],
+      },    
 };
 
 export default nextConfig;
